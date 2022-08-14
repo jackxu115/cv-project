22d8e5700dcaa76170560453d8f6562c755b3827
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-08-13 22:33:35 -0700

    add commpoent and css folder

---
+++ src/components/GeneralInfo.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class GeneralInfo extends Component {
+    render() {
+        return (
+            <div>
+                <label htmlFor=""></label>
+            </div>
+        )
+    }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>CV Generator</h1>
+                <button>Preview</button>
+            </div>
+        )
+    }
+}
+
+export default Header
